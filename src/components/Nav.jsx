6dfc75632acc456b0e43e92df266318adb3d3a90
Taf <tijn.f@yahoo.com>
2026-03-03 13:14:50 +0100

    added main page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import Noorderpoort from "./../assets/noorderpoort-dark.png";
 export default function Nav() {
     return (
-        <nav className="bg-black shadow-np border-b-4 border-np p-2 text-xl text-white flex justify-center">
+        <nav className="bg-black shadow-np border-b-4 sticky z-1000 top-0 border-np p-2 text-xl text-white flex justify-center">
             <div className="flex items-center justify-between flex-row w-full max-w-[74rem]">
                 {" "}
                 <div className="flex flex-row items-center">
                     <a href="/">
-                        <img className="w-70 px-2" src={Noorderpoort} alt="noorderpoort" />
+                        <img className="w-60 px-2" src={Noorderpoort} alt="noorderpoort" />
                     </a>
-                    <div className="text-2xl uppercase font-roboto font-bold">
+                    <div className="text-2xl font-roboto font-bold">
                         <Link className="mx-1 font-roboto" to="/">
                             Home
                         </Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,23 @@ export default function HomePage() {
         <>
             <Nav></Nav>
             <div className="px-[20rem] py-3">
-                <div className="bg-white border border-black/10 shadow-np rounded-lg my-3 p-2">
-                    <Heading title="Test"></Heading>
-                    <Comment></Comment>
-                    <ListItem place={1} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                <Heading title="Films"></Heading>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+                    <div className="md:order-2">
+                        <h3 className="text-2xl font-bold">Kies uit catagorie</h3>
+                    </div>
+
+                    <div className="col-span-2 md:order-1">
+                        <ListItem place={1} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={2} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={3} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={4} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={6} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={7} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={8} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={9} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                        <ListItem place={10} rating={9} img="https://placehold.co/400x600" item="Film"></ListItem>
+                    </div>
                 </div>
             </div>
         </>
