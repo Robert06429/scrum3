a2c68516d3f35292def2c114aa29d419b2ed199c
Robert06429 <rgrslinkman@st.noorderpoort.nl>
2026-03-05 08:48:40 +0100

    de back-end van de upload pagina is klaar en de login is van plek everpaatst en een sesion gestart en een paar andere klijne verbeteringen toegevoegt

--- src/components/login.php
+++ src/components/login.php
@@ -4,7 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json; charset=utf-8');
-session_start();
 
 // Handle preflight requests
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
@@ -18,10 +17,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents("php://input"));
     $email = $data->email;
     $password = $data->password;
+    if (!$password || !$email) {
+        echo json_encode(['message' => 'Password and email are required']);
+        exit();
+    }
+
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
     $stmt->execute(['email' => $email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && $password === $user['password']) { // password_verify($password, $user['password']) for hashed passwords
         echo json_encode(['message' => 'Login successful']);
         $_SESSION["user_id"] = $user['id'];
         $_SESSION["username"] = $user['name'];
--- src/components/upload.php
+++ src/components/upload.php
@@ -5,6 +5,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Author');
+session_start();
 
 $error = "";
 
