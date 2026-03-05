d490ae46daa0ca9a4159565b873c15972229f981
fernando bos <fdmbos@st.noorderpoort.nl>
2026-03-05 09:46:39 +0100

    fix

--- src/components/main.php
+++ src/components/main.php
@@ -1,6 +1,9 @@
 <?php
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 include_once 'db/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
