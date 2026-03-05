65b04fdaaf353f03548cf095b87c9e2aa8b9ebd4
Robert06429 <rgrslinkman@st.noorderpoort.nl>
2026-03-05 09:15:02 +0100

    ik heb gecheckt of een session leeg is en dat het word door gegeven aan de front-end

--- src/components/uitloggen.php
+++ src/components/uitloggen.php
@@ -2,5 +2,12 @@
 
 session_start();
 session_destroy();
+
+if (empty($_SESSION)) {
+    echo  json_encode("Session is destroyed of leeg");
+} else {
+    echo  json_encode("Session bestaat nog");
+}
+
 exit();
 ?>
